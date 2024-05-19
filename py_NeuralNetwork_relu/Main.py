a3e0c77110b5ce3dc630970911a5e3626e673205
Kenny Guo <kennykguo@gmail.com>
2024-05-19 15:59:39 -0400

    changes

--- py_NeuralNetwork_relu/Main.py
+++ py_NeuralNetwork_relu/Main.py
@@ -9,6 +9,4 @@ Data = Dataset('train.csv')
 Data.process_data()
 Model = Network()
 Model.stochastic_gradient_descent(Data.X_train, Data.Y_train, Data.X_dev, Data.Y_dev, 30, 50, 0.01)
-
-
 np.savez("model_parameters.npz", Model.dW1, Model.b1, dW2=Model.W2, db2=Model.db2)
