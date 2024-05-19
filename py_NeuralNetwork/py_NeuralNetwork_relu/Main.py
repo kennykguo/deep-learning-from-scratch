87e369399d3fa442daa5dde7579d7ae0073016a3
Kenny Guo <kennykguo@gmail.com>
2024-05-19 18:27:45 -0400

    update

---
+++ py_NeuralNetwork/py_NeuralNetwork_relu/Main.py
@@ -0,0 +1,19 @@
+from Network import Network
+from Dataset import Dataset
+import torch
+import numpy as np
+import pandas as pd
+import os
+
+# Interface
+Data = Dataset('train.csv')
+
+Data.process_data()
+
+Model = Network()
+
+Model.stochastic_gradient_descent(Data.X_train, Data.Y_train, Data.X_dev, Data.Y_dev, 5, 20, 0.01)
+
+np.savez("model_parameters.npz", Model.dW1, Model.b1, dW2=Model.W2, db2=Model.db2)
+
+Model.plot_accuracies()
